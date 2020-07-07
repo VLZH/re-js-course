1d552312ec7abb59d21f092b72feae621e767291
vlzh <vlzh@pm.me>
2020-07-08 05:19:42 +0600

    Add some pages from notion.so

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,54 +1,59 @@
 module.exports = {
-  title: 'Javascript [ITR]',
-  tagline: 'Javascript tutorial for ITR',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  favicon: '/img/logo.svg',
-  organizationName: 'ITR', // Usually your GitHub org/user name.
-  projectName: 'Javascript [ITR]', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'You dont know JS',
-      logo: {
-        alt: 'Javascript [ITR]',
-        src: '/img/logo.svg',
-      },
-      links: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+    title: "Javascript [ITR]",
+    tagline: "Javascript tutorial for ITR",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    favicon: "/img/logo.svg",
+    organizationName: "ITR", // Usually your GitHub org/user name.
+    projectName: "Javascript [ITR]", // Usually your repo name.
+    themeConfig: {
+        prism: {
+            theme: require("prism-react-renderer/themes/github"),
+            darkTheme: require("prism-react-renderer/themes/dracula"),
         },
-        {
-          href: 'https://github.com/VLZH/js-course',
-          label: 'GitHub',
-          position: 'right',
+        navbar: {
+            title: "You dont know JS",
+            logo: {
+                alt: "Javascript [ITR]",
+                src: "/img/logo.svg",
+            },
+            links: [
+                {
+                    to: "docs/",
+                    activeBasePath: "docs",
+                    label: "Docs",
+                    position: "left",
+                },
+                {
+                    href: "https://github.com/VLZH/js-course",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
         },
-      ],
+        footer: {
+            style: "dark",
+            links: [],
+            copyright: `Copyright © ${new Date().getFullYear()} https://github/VLZH. Built with Docusaurus🦖.`,
+        },
     },
-    footer: {
-      style: 'dark',
-      links: [],
-        copyright: `Copyright © ${new Date().getFullYear()} https://github/VLZH. Built with Docusaurus🦖.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          // It is recommended to set document id as docs home page (`docs/` path).
-            homePageId: 'main',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/vlzh/js-course/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                docs: {
+                    routeBasePath: "/",
+                    homePageId: "main",
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/vlzh/js-course/",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            },
+        ],
     ],
-  ],
+    themes: ["@docusaurus/theme-live-codeblock"],
+    plugins: ["@docusaurus/plugin-ideal-image"],
 };
--- sidebars.js
+++ sidebars.js
@@ -1,18 +1,22 @@
 module.exports = {
-  main_sidebar: {
-    Основное: ["main"],
-    Javascript: [
-      "js/comments_statements_expressions",
-      {
-        type: "category",
-        label: "Операторы",
-        items: [
-          "js/operators/arithmetic_operators",
-          "js/operators/assigment_operators",
-          "js/operators/bitwise_operators",
-          "js/operators/comparison_logical_operators",
+    main_sidebar: {
+        Основное: ["main"],
+        Javascript: [
+            "js/comments_statements_expressions",
+            "js/basic_statements",
+            {
+                type: "category",
+                label: "Операторы",
+                items: [
+                    "js/operators/arithmetic_operators",
+                    "js/operators/assigment_operators",
+                    "js/operators/bitwise_operators",
+                    "js/operators/comparison_logical_operators",
+                ],
+            },
+            "js/variables",
+            "js/data_types",
+            "js/functions",
         ],
-      },
-    ],
-  },
+    },
 };
